94df0a20167cbf54e1e634eb8d1e05aa65c1b0f8
mulgey <mustafaulgey@hotmail.com>
2023-07-26 11:12:52 +0300

    update please

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
-import { Text } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+// components
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Welcome">
+        <Drawer.Screen name="User" component={UserScreen} />
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ headerStyle: { backgroundColor: "#cccccc" } }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
